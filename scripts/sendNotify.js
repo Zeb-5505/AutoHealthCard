c070087c0081ac7c2bab74c0f16f6fb00d9fadb6
Zeb-5505 <89502452+Zeb-5505@users.noreply.github.com>
2022-06-05 18:09:42 +0800

    Update sendNotify.js
    改企微推送（未完成）

--- scripts/sendNotify.js
+++ scripts/sendNotify.js
@@ -1,8 +1,44 @@
 const got = require('got')
 
-const SCKEY = process.env.SCKEY
+//const SCKEY = process.env.SCKEY
+//企微的参数
+const corpid = process.env.CKEY
+const corpsecret = process.env.SKEY
+
 
 const sendNotify = (title, message) => {
+  //TODO: 企业微信推送
+      try {
+         if (corpid && corpsecret) {
+                let url = `https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=${corpid}&corpsecret=${corpsecret}`
+                let res = await axios.get(url)
+                access_token = res.data.access_token
+                let turl = `https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=${access_token}`
+                let data = {
+                    "touser": "@all",
+                    "msgtype": "text",
+                    "agentid": 1000002,
+                    "text": {
+                        "content": msg
+                    },
+                    "safe": 0
+                }
+                let tres = await axios.post(turl, data)
+                if (tres.data.errcode == 0) {
+                    console.log("企业微信:发送成功");
+                } else {
+                    console.log("企业微信:发送失败");
+                    console.log(tres.data.errmsg);
+                }
+            } else {
+                console.log("企业微信:你还没有填写corpsecret和corpid呢，推送个鸡腿");
+            }
+        } catch (err) {
+            console.log("企业微信：发送接口调用失败");
+            console.log(err);
+        }
+        resolve();
+  /*
   if (SCKEY) {
     got(`https://sc.ftqq.com/${SCKEY}.sendNotify`, {
       searchParams: {
@@ -27,6 +63,7 @@ const sendNotify = (title, message) => {
   } else {
     console.log('您未提供server酱的SCKEY，取消微信推送消息通知\n')
   }
+  */
 }
 
 module.exports = sendNotify
